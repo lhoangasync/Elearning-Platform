9d5915992324925f8967197d2a21f5422078ce3a
Le Hoang <vatcvietmy123456@gmail.com>
2025-11-20 21:03:47 +0700

    update

--- ai-server/src/config/env.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-export const env = {
-  geminiApiKey: process.env.GOOGLE_GEMINI_API_KEY || "",
-};
-
-if (!env.geminiApiKey) {
-  throw new Error("GOOGLE_GEMINI_API_KEY is not set in environment variables");
-}
